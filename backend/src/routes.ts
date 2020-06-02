00534d651b799278a08972a39887fe1ff76109e8
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-06-02 20:52:59 -0300

    feat: list items

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express';
+import knex from './database/connection';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
   return response.json({ msg: 'Server Up' });
+});
+
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(({ title, image }) => {
+    return {
+      title,
+      image_url: `http://localhost:3002/uploads/${image}`,
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
