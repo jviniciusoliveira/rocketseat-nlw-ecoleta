715fda43f375ddf011d02c88d2ba7962bdb52cb4
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-06-04 19:59:54 -0300

    feat: home component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Ecoleta</h1>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
