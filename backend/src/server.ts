391a940cde0f6eaeabc0decc169a47e9cd346612
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-06-04 21:18:33 -0300

    feat: add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 import { resolve } from 'path';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
