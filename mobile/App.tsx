f55c2c3ce29ab5b0f0706cd80b95d16cff3c13d4
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-06-07 16:45:14 -0300

    project: initial commit mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ecoleta</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
