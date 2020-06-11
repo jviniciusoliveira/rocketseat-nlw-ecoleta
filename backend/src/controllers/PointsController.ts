f0dd72740d09f9ab2d469fb11ad5b91b30ae3095
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-06-11 19:39:20 -0300

    feat: backend - upload files

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -18,7 +18,14 @@ class PointsController {
       .distinct()
       .select('points.*');
 
-    return response.json(points);
+    const serializedPoints = points.map((point) => {
+      return {
+        ...point,
+        image_url: `http://192.168.0.13:3002/uploads/${point.image}`,
+      };
+    });
+
+    return response.json(serializedPoints);
   }
 
   async show(request: Request, response: Response) {
@@ -30,12 +37,17 @@ class PointsController {
       return response.status(400).json({ error: 'Point not found.' });
     }
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://192.168.0.13:3002/uploads/${point.image}`,
+    };
+
     const pointItems = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title', 'items.image');
 
-    return response.json({ point: { ...point }, items: pointItems });
+    return response.json({ point: serializedPoint, items: pointItems });
   }
 
   async create(request: Request, response: Response) {
@@ -47,7 +59,6 @@ class PointsController {
       longitude,
       city,
       uf,
-      image,
       items,
     } = request.body;
 
@@ -62,15 +73,18 @@ class PointsController {
         longitude,
         city,
         uf,
-        image,
+        image: request.file.filename,
       });
 
-      const pointItems = items.map((item_id: Number) => {
-        return {
-          item_id,
-          point_id,
-        };
-      });
+      const pointItems = items
+        .split(',')
+        .map((item: string) => Number(item.trim()))
+        .map((item_id: Number) => {
+          return {
+            item_id,
+            point_id,
+          };
+        });
 
       await trx('point_items').insert(pointItems);
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (request, response) => {
   const serverInfo = {
@@ -21,6 +24,6 @@ routes.get('/items', ItemsController.index);
 
 routes.get('/points', PointsController.index);
 routes.get('/points/:id', PointsController.show);
-routes.post('/points', PointsController.create);
+routes.post('/points', upload.single('image'), PointsController.create);
 
 export default routes;
